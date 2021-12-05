ac794acb89a54138bb5fe39725ec8986a5fee7cc
adrian@basave.com <adrian@basave.com>
2021-12-05 18:46:10 +0100

    removed credentials from project

--- src/constants/credentials.ts
+++
@@ -1,9 +0,0 @@
-const credentials = {
-	token: "ODIyNTU5MjgzMTMxOTczNjQz.YFUCBw.z2hI3zY2mb3dwctD20WbNrSBOkI",
-	permissions: "2148005952",
-	clientId: "822559283131973643",
-	publicKey: "730aaf019d0615f3341d19a85271279324b30438af375b36f2dbe2acc2dd41af",
-	url: "https://discord.com/oauth2/authorize?client_id=822559283131973643&scope=bot&permissions=2147483647",
-}
-
-export default credentials
